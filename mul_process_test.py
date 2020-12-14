d1be562b9b754f09f50ff8a4055889454821e060
skysnow <skysnow2017@gmail.com>
2020-12-14 12:43:48 +0800

    add mul_process

--- mul_process_test.py
+++ mul_process_test.py
@@ -11,31 +11,39 @@ from concurrent.futures import ThreadPoolExecutor
 import time
 import logging
 import threading
+
 logging.basicConfig(level=logging.INFO, format='[%(threadName)-9s] %(message)s')
 
 max_workers_number = 3
 
 task_queue = Queue()
+start_time = time.time()
 
 
 def productor(paths: list):
     executor = ThreadPoolExecutor(max_workers=max_workers_number)
     for result in executor.map(get_item, paths):
         task_queue.put(result)
-        logging.info('{} already add the queue in {}'.format(result,threading.current_thread().ident))
+        logging.info('{} already add the queue in {}'.format(result, threading.current_thread().ident))
+        print(start_time - time.time())
 
-def consumer(queue:Queue):
-    task_list=[]
+
+def consumer(queue: Queue):
+    start_time = time.time()
+    task_list = []
     while True:
         if queue and queue.not_empty:
             task_list.append(queue.get())
             executor = ThreadPoolExecutor(max_workers=max_workers_number)
-            for result in executor.map(get_item,task_list ):
-                    logging.info('{} already remove the queue in {}'.format(result,threading.current_thread().ident))
+            for result in executor.map(get_item, task_list):
+                logging.info('{} already remove the queue in {}'.format(result, threading.current_thread().ident))
+                print(start_time - time.time())
+
 
 def get_item(name):
     time.sleep(3)
     return name
+
 
 test_list = [1, 2, 3, 4, 5, 6]
 productor(test_list)
